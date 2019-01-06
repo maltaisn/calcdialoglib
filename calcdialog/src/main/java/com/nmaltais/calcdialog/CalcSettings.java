ad6097094349f28acfcefcf59001bcd10f4b30de
Daniel Guger <guger.daniel@gmail.com>
2019-01-06 14:29:19 +0100

    Updated Component versions, AndroidX-Migration (Closes #12)

--- calcdialog/src/main/java/com/nmaltais/calcdialog/CalcSettings.java
+++ calcdialog/src/main/java/com/nmaltais/calcdialog/CalcSettings.java
@@ -1,7 +1,7 @@
 package com.nmaltais.calcdialog;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
