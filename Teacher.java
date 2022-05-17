db5fa901386f21880cc432b23cb445832e70677c
Aartemis899 <nafisjamil72@gmail.com>
2022-05-18 03:13:26 +0600

    version 2

---
+++ Teacher.java
@@ -0,0 +1,19 @@
+public class Teacher{
+	public String name;
+	public int id;
+
+	public Teacher (String str, int i){
+		name=str;
+		id=i;
+	}
+	public Teacher(){
+
+	}
+
+	public void setName (String str) {
+        name=str;
+	}
+	public String getName (){
+		return name;
+	}
+}
